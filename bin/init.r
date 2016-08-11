aed73b23c32870d127f88c75577516423abf1190
NaShan <shanna@Shannas-MacBook-Pro.local>
2016-08-11 15:33:44 +0800

    init.r

---
+++ bin/init.r
@@ -0,0 +1,15 @@
+r <- getOption("repos")
+r["CRAN"] <- "http://cran.us.r-project.org"
+options(repos=r)
+.libPaths("./r_library")
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("graph")
+biocLite("Rgraphviz")
+biocLite("RBGL")
+
+install.packages("Rook", dependencies=T , clean=T)
+install.packages("hash", dependencies=T , clean=T)
+install.packages("gRain", dependencies=T , clean=T)
+install.packages("gRbase", dependencies=T , clean=T)
+install.packages("jsonlite", dependencies=T , clean=T)
